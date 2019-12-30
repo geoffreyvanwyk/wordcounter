c68d21e3997270068e1b6630da757adaf8c5fef0
Geoffrey van Wyk <geoffrey@vanwyk.biz>
2019-12-30 03:26:19 +0200

    fix:  Line count file with nonblank last line

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,24 +1,43 @@
 import {expect, test} from '@oclif/test'
-
-import cmd = require('../src')
 import {execSync} from 'child_process'
 
+import cmd = require('../src')
+
 describe('@systemovich/wordcounter', () => {
-  const testFile = 'test/fixtures/testfile.txt'
+  const fixturesDir = 'test/fixtures/'
+
+  const testFile = {
+    blank: fixturesDir.concat('blank.txt'),
+    lastLine: {
+      blank: fixturesDir.concat('blank_last_line.txt'),
+      nonblank: fixturesDir.concat('nonblank_last_line.txt'),
+    },
+  }
 
   test
   .stdout()
-  .add('wc', execSync(`wc --lines ${testFile}`).toString())
-  .do(() => cmd.run(['--lines', testFile]))
-  .it('prints the newline counts with long flag name', ctx => {
+  .add('wc', execSync(`wc --lines ${testFile.blank}`).toString())
+  .do(() => cmd.run(['--lines', testFile.blank]))
+  .it('prints the newline counts when file is blank', ctx => {
     expect(ctx.stdout).to.equal(ctx.wc)
   })
 
   test
   .stdout()
-  .add('wc', execSync(`wc -l ${testFile}`).toString())
-  .do(() => cmd.run(['-l', testFile]))
-  .it('prints the newline counts with short flag name', ctx => {
+  .add('wc', execSync(`wc --lines ${testFile.lastLine.blank}`).toString())
+  .do(() => cmd.run(['--lines', testFile.lastLine.blank]))
+  .it('prints the newline counts when last line is blank', ctx => {
     expect(ctx.stdout).to.equal(ctx.wc)
+  })
+
+  test
+  .stdout()
+  .add('wc', execSync(`wc --lines ${testFile.lastLine.nonblank}`).toString())
+  .do(() => cmd.run(['--lines', testFile.lastLine.nonblank]))
+  .it('prints the newline counts when last line is not blank', ctx => {
+    const count = parseInt(ctx.wc, 10) + 1
+    const rest = ctx.wc.replace(/^\d+\s/, '')
+
+    expect(ctx.stdout).to.equal(count.toString().concat(' ').concat(rest))
   })
 })
