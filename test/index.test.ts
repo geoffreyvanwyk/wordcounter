545e7328a1b4f60c741ed2d6b26ca2af338e81e3
Geoffrey van Wyk <geoffrey@vanwyk.biz>
2019-12-31 01:38:57 +0200

    test: Make tests independent of wc

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,43 +1,27 @@
 import {expect, test} from '@oclif/test'
-import {execSync} from 'child_process'
 
 import cmd = require('../src')
 
 describe('@systemovich/wordcounter', () => {
   const fixturesDir = 'test/fixtures/'
-
-  const testFile = {
-    blank: fixturesDir.concat('blank.txt'),
-    lastLine: {
-      blank: fixturesDir.concat('blank_last_line.txt'),
-      nonblank: fixturesDir.concat('nonblank_last_line.txt'),
-    },
-  }
+  const blankFile = fixturesDir.concat('blank.txt')
+  const testFile = fixturesDir.concat('testfile.txt')
 
   test
   .stdout()
-  .add('wc', execSync(`wc --lines ${testFile.blank}`).toString())
-  .do(() => cmd.run(['--lines', testFile.blank]))
-  .it('prints the newline counts when file is blank', ctx => {
-    expect(ctx.stdout).to.equal(ctx.wc)
+  .do(() => cmd.run(['--lines', blankFile]))
+  .it('prints 0 for the newline count of a blank file', ctx => {
+    expect(ctx.stdout).to.equal(
+      (0).toString().concat(' ').concat(blankFile).concat('\n')
+    )
   })
 
   test
   .stdout()
-  .add('wc', execSync(`wc --lines ${testFile.lastLine.blank}`).toString())
-  .do(() => cmd.run(['--lines', testFile.lastLine.blank]))
-  .it('prints the newline counts when last line is blank', ctx => {
-    expect(ctx.stdout).to.equal(ctx.wc)
-  })
-
-  test
-  .stdout()
-  .add('wc', execSync(`wc --lines ${testFile.lastLine.nonblank}`).toString())
-  .do(() => cmd.run(['--lines', testFile.lastLine.nonblank]))
-  .it('prints the newline counts when last line is not blank', ctx => {
-    const count = parseInt(ctx.wc, 10) + 1
-    const rest = ctx.wc.replace(/^\d+\s/, '')
-
-    expect(ctx.stdout).to.equal(count.toString().concat(' ').concat(rest))
+  .do(() => cmd.run(['--lines', testFile]))
+  .it('prints the newline counts for a nonempty file', ctx => {
+    expect(ctx.stdout).to.equal(
+      (29).toString().concat(' ').concat(testFile).concat('\n')
+    )
   })
 })
